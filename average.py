4a3b8d80e68fd4e2abfb7313279a6514cf2f3493
Xavier Melancon <xavier.e.melancon@gmail.com>
2025-01-21 08:16:58 -0800

    Upload

--- average.py
+++ average.py
@@ -12,7 +12,7 @@ def test_compute_average():
     values = [random.randint(0,100) for _ in range(10)]
     assert compute_average(values)==55.3
 def main():
-        print(compute_average([1,2,3,7,8,33,12,-2]))
+        print(compute_average([1,2,3,7,8,33,12,-2])) #Should be 8.0
 
 if __name__=="__main__":
      main()
